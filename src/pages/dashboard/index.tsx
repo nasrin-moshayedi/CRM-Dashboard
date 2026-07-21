7d6633a9f1a2536c039035976827d86477872544
nasrin-moshayedi <n.mosh4yedi@gmail.com>
2026-07-21 18:45:10 +0330

    feat(dashboard): build initial dashboard structure

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -15,6 +15,37 @@ const Dashboard = ({setMenu, menu}: DashboardProps) => {
     return(
         <>
         <Header menu={menu} setMenu={setMenu} />
+        <div className="flex w-100 gap-16 dahsboad-content">
+            <div className="flex w-100 gap-16 direction-column">
+                <div className="flex gap-16 w-100">
+                    <Card className="w-60">chart</Card>
+                    <Card className="w-40">AI suggestion</Card>
+                </div>
+                <div className="flex gap-16 w-100">
+                    <Card className="w-25">create</Card>
+                    <Card className="w-25">add</Card>
+                    <Card className="w-25">new</Card>
+                    <Card className="w-25">report</Card>
+                </div>
+                
+            </div>
+            <div className="flex gap-16 w-45 direction-column">
+                <Card>overview</Card>
+                <Card>status</Card>
+            </div>
+        </div>
+        <div className="grid grid-3 gap-16 w-100 dahsboad-content">
+            <Card className="w-100">create</Card>
+            <Card className="w-100">add</Card>
+            <Card className="ww-100">new</Card>
+        </div>
+        <div className="grid grid-4 gap-16 w-100 dahsboad-content">
+            <Card className="w-100">create</Card>
+            <Card className="w-100">add</Card>
+            <Card className="ww-100">new</Card>
+            <Card className="ww-100">new</Card>
+        </div>
+        
        </>
     )
 }
